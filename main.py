1c1ed001056ca661478ec1739776165359f1ca8c
far <kitwhalle@gmail.com>
2023-01-11 00:59:43 +0300

    v1 Added proper graph rotation

--- main.py
+++ main.py
@@ -4,53 +4,70 @@ import sys
 import pygame
 
 
-def project_vertex(vertex: list[int, int, int], focal_length: int, width: int, height: int):
+def multiply_matrix(a, b):
+    a_rows = len(a)
+    a_cols = len(a[0])
+    b_rows = len(b)
+    b_cols = len(b[0])
+    product = [[0 for i in range(b_cols)] for i in range(a_rows)]
+    if a_cols == b_rows:
+        for i in range(a_rows):
+            for j in range(b_cols):
+                for k in range(b_rows):
+                    product[i][j] += a[i][k] * b[k][j]
+    return product
+
+
+def project_vertex(vertex, focal_length: int, width: int, height: int):
     x, y, z = vertex
-    x_projected = (focal_length * x) // (z + focal_length)
-    y_projected = (focal_length * y) // (z + focal_length)
+    x_projected = (focal_length * x[0]) // (z[0] + focal_length)
+    y_projected = (focal_length * y[0]) // (z[0] + focal_length)
     return [x_projected + width // 2, y_projected + height // 2]
 
 
-def rotate(vertex: list[int, int, int], pitch: int, roll: int, yaw: int):
-    x, y, z = vertex
-    new_x = x * (cos(pitch) * cos(roll)) + y * (sin(yaw) * sin(pitch) * cos(roll) - cos(yaw) * sin(roll)) + z * (cos(yaw) * sin(pitch) * cos(roll) + sin(yaw) * sin(roll))
-    new_y = x * (cos(pitch) * sin(roll)) + y * (sin(yaw) * sin(pitch) * sin(roll) + cos(yaw) * cos(roll)) + z * (cos(yaw) * sin(pitch) - sin(roll) + sin(yaw) * cos(roll))
-    new_z = x * (-sin(pitch)) + y * (sin(yaw) * cos(pitch)) + z * (cos(yaw) * cos(pitch))
-    return [new_x, new_y, new_z]
+def rotate(vertex, pitch, yaw, roll):
+    return multiply_matrix([[cos(roll), -sin(roll), 0], [sin(roll), cos(roll), 0], [0, 0, 1]], multiply_matrix([[cos(yaw), 0, sin(yaw)], [0, 1, 0], [-sin(yaw), 0, cos(yaw)]], multiply_matrix([[1, 0, 0], [0, cos(pitch), -sin(pitch)], [0, sin(pitch), cos(pitch)]], vertex)))
 
 
 def main():
     width = 800
     height = 600
     black = [0, 0, 0]
-    green = [0, 255, 0]
-    vertexes = [[-100, -100, 200], [100, -100, 200], [100, 100, 200], [-100, 100, 200],
-                [-100, -100, 0], [100, -100, 0], [100, 100, 0], [-100, 100, 0]]
+    white = [255, 255, 255]
+    red = [255, 0, 0]
+    vertexes = [[[-100], [-100], [100]], [[100], [-100], [100]], [[100], [100], [100]], [[-100], [100], [100]],
+                [[-100], [-100], [-100]], [[100], [-100], [-100]], [[100], [100], [-100]], [[-100], [100], [-100]]]
     edges = {0: {1, 3, 4},
+             1: {0, 2, 5},
              2: {1, 3, 6},
+             3: {0, 2, 7},
+             4: {0, 5, 7},
              5: {1, 4, 6},
+             6: {2, 5, 7},
              7: {3, 4, 6}}
 
-    # pygame.init()
-    # screen = pygame.display.set_mode([width, height])
+    pygame.init()
+    screen = pygame.display.set_mode([width, height])
     game_over = False
     while not game_over:
-        # Обработка событий
-        # for event in pygame.event.get():
-        #     if event.type == pygame.QUIT:
-        #         game_over = True
+        # Event handling
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                game_over = True
 
-        # Вычисления
-        vertexes = list(map(lambda vertex: rotate(vertex, 0, 0, 3), vertexes))
+        # Calculate
+        vertexes = list(map(lambda vertex: rotate(vertex, 0, 0.025, 0), vertexes))
         vertexes_projected = list(map(lambda vertex: project_vertex(vertex, 400, width, height), vertexes))
 
-        # Отрисовка
-        # screen.fill(black)
-        # for i in edges.keys():
-        #     for j in edges[i]:
-        #         pygame.draw.line(screen, green, vertexes_projected[i], vertexes_projected[j], 5)
-        # pygame.display.flip()
-        # pygame.time.wait(10)
+        # Frame drawing
+        screen.fill(black)
+        for i in edges.keys():
+            pygame.draw.circle(screen, red, vertexes_projected[i], 6, 0)
+        for i in [0, 2, 5, 7]:
+            for j in edges[i]:
+                pygame.draw.line(screen, white, vertexes_projected[i], vertexes_projected[j], 3)
+        pygame.display.flip()
+        pygame.time.wait(10)
 
     sys.exit(0)
 
